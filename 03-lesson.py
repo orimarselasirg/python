523da8a6e31f2603e90fd0e5ed95e27cc719c301
Ramiro Grisales <ramirogrisales@gmail.com>
2025-04-24 19:34:10 -0500

    third lesson

---
+++ 03-lesson.py
@@ -0,0 +1,15 @@
+lista = []
+
+lista.append("hola")
+lista.append("otro elemento")
+lista.append("tercer elemento")
+print(lista)
+# lista.insert(1,"prueba")
+# lista.remove("otro elemento")
+lista.pop()
+
+
+
+
+
+print(lista)
